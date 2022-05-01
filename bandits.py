28126fd043a27aee46f4ae6aae68ef8a4f83f531
lhotaane <74431089+lhotaane@users.noreply.github.com>
2022-05-01 23:38:59 +0200

    Experiment
    Finished code for comparison

--- bandits.py
+++ bandits.py
@@ -274,3 +274,114 @@ class softmax_bandit:
         self.reward = np.zeros(iters)
         self.k_reward = np.zeros(k)
         self.probabilities = np.zeros(k)
+
+
+class annealing_softmax_bandit:
+    '''
+    SoftMax k-bandit problem using Simulated 
+    
+    Inputs
+    =====================================================
+    k: number of arms (int)
+    iters: number of steps (int)
+    T: temperature 0 <= T (float)
+    mu: set the average rewards for each of the k-arms.
+        Set to "random" for the means to be selected from
+        a normal distribution with mean = 0. 
+        Set to "sequence" for the means to be ordered from 
+        0 to k-1 by 0.
+        Set to "sequence2" for the means to be orderes from
+        0 to (k-1)/2 by 0.5.
+        Pass a list or array of length = k for user-defined
+        values.
+    '''
+    
+    def __init__(self, k, iters, mu='random',cooling='log'):
+        # Number of arms
+        self.k = k
+        # Number of iterations
+        self.iters = iters
+        # Step count
+        self.n = 0
+        # Step count for each arm
+        self.k_n = np.zeros(k)
+        # Total mean reward
+        self.mean_reward = 0
+        self.reward = np.zeros(iters)
+        # Mean reward for each arm
+        self.k_reward = np.zeros(k)
+        # Temperature
+        self.T = 0
+        # Selection probabilities
+        self.probabilities = np.zeros(k)
+        # Colling the temperature
+        self.cooling = 'log'
+
+        # Define mean for each arm
+        if type(mu) == list or type(mu).__module__ == np.__name__:
+            # User-defined averages            
+            self.mu = np.array(mu)
+        elif mu == 'random':
+            # Draw means from probability distribution
+            self.mu = np.random.normal(0, 1, k)
+        elif mu == 'sequence':
+            # Increase the mean for each arm by one
+            self.mu = np.linspace(0, k-1, k)
+        elif mu == 'sequence2':
+            # Increase the mean for each arm by one
+            self.mu = np.linspace(0, k-1, k)/2
+        
+    def pull(self):
+
+        # Update temperature
+        self.update_temperature()
+
+        # Probabilities distributed according to mean reward
+        self.probabilities= np.exp(self.k_reward/self.T)
+        self.probabilities = self.probabilities/np.sum(self.probabilities)
+        probabilities_cmsm = np.cumsum(self.probabilities)
+
+        # With Boltzman probability select an arm
+        p=np.random.rand()
+        probabilities_cmsm_b = probabilities_cmsm[probabilities_cmsm>p][0]
+        a = np.argmax(probabilities_cmsm == probabilities_cmsm_b)
+        
+        reward = np.random.normal(self.mu[a], 1)
+        
+        # Update counts
+        self.n += 1
+        self.k_n[a] += 1
+        
+        # Update total
+        self.mean_reward = self.mean_reward + (
+            reward - self.mean_reward) / self.n
+        
+        # Update results for a_k
+        self.k_reward[a] = self.k_reward[a] + (
+            reward - self.k_reward[a]) / self.k_n[a]
+        
+    def run(self):
+        for i in range(self.iters):
+            self.pull()
+            self.reward[i] = self.mean_reward
+            
+    def reset(self):
+        # Resets results while keeping settings
+        self.n = 0
+        self.k_n = np.zeros(k)
+        self.mean_reward = 0
+        self.reward = np.zeros(iters)
+        self.k_reward = np.zeros(k)
+        self.probabilities = np.zeros(k)
+        self.T = 0
+
+    def update_temperature(self):
+        t = 1 + self.n
+
+        if self.cooling == 'log':
+            self.T = 1/np.log(t+0.0000001)
+        elif self.coooling == 'abs':
+            self.T = 1/t
+        elif self.cooling == 'random':
+            r= np.random.rand(1)
+            self.T = 1/(r+0.0000001)
